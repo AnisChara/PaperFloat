84cbcfb9bc2f16a0346128e1fca3f4996f0b99c1
Chara Anis <chara@et.esiea.fr>
2023-11-22 10:47:46 +0100

    liste des bateaux mieux rédiger

--- m_liste_bateaux.js
+++ m_liste_bateaux.js
@@ -1,64 +1,54 @@
 "use strict"
 
 let bateau_2_A = [];
-let case_2_A = []
 let bateau_2_B = [];
-let case_2_B = []
 let bateau_3_A = [];
-let case_3_A = []
 let bateau_3_B = [];
-let case_3_B = []
 let bateau_4 = [];
-let case_4 = []
 let bateau_10 = [];
-let case_10 = []
 
 let bateaux = [
 
-    bateau_2_A = case_2_A =[ 
-        case_2_A[0] = {x : 0, y : 0, state : false},
-        case_2_A[1] = {x : 0, y : 0, state : false}
+    bateau_2_A = [
+        {x : 0, y : 0, state : false},
+        {x : 0, y : 0, state : false}
     ],
 
-    bateau_2_B = case_2_B =[ 
-        case_2_B[0] = {x : 0, y : 0, state : false},
-        case_2_B[1] = {x : 0, y : 0, state : false}
+    bateau_2_B = [ 
+        {x : 0, y : 0, state : false},
+        {x : 0, y : 0, state : false}
     ],
 
-    bateau_3_A = case_3_A =[
-        case_3_A[0] = {x : 0, y : 0, state : false},
-        case_3_A[0] = {x : 0, y : 0, state : false},
-        case_3_A[1] = {x : 0, y : 0, state : false}
+    bateau_3_A = [
+        {x : 0, y : 0, state : false},
+        {x : 0, y : 0, state : false},
+        {x : 0, y : 0, state : false}
     ],
 
-    bateau_3_B = case_3_B =[
-        case_3_B[0] = {x : 0, y : 0, state : false},
-        case_3_B[0] = {x : 0, y : 0, state : false},
-        case_3_B[0] = {x : 0, y : 0, state : false}
+    bateau_3_B = [
+        {x : 0, y : 0, state : false},
+        {x : 0, y : 0, state : false},
+        {x : 0, y : 0, state : false}
     ],
     
-    bateau_4 = case_4 =[
-        case_4[0] = {x : 0, y : 0, state : false},
-        case_4[0] = {x : 0, y : 0, state : false},
-        case_4[0] = {x : 0, y : 0, state : false},
-        case_4[0] = {x : 0, y : 0, state : false},
+    bateau_4 = [
+        {x : 0, y : 0, state : false},
+        {x : 0, y : 0, state : false},
+        {x : 0, y : 0, state : false},
+        {x : 0, y : 0, state : false}
     ],
 
-    bateau_10 = case_10 =[
-        case_10[0] = {x : 0, y : 0, state : false},
-        case_10[0] = {x : 0, y : 0, state : false},
-        case_10[0] = {x : 0, y : 0, state : false},
-        case_10[0] = {x : 0, y : 0, state : false},
-        case_10[0] = {x : 0, y : 0, state : false},
-        case_10[0] = {x : 0, y : 0, state : false},
-        case_10[0] = {x : 0, y : 0, state : false},
-        case_10[0] = {x : 0, y : 0, state : false},
-        case_10[0] = {x : 0, y : 0, state : false},
-        case_10[0] = {x : 0, y : 0, state : false},
-
+    bateau_10 =[
+        {x : 0, y : 0, state : false},
+        {x : 0, y : 0, state : false},
+        {x : 0, y : 0, state : false},
+        {x : 0, y : 0, state : false},
+        {x : 0, y : 0, state : false},
+        {x : 0, y : 0, state : false},
+        {x : 0, y : 0, state : false},
+        {x : 0, y : 0, state : false},
+        {x : 0, y : 0, state : false},
+        {x : 0, y : 0, state : false},
     ]
 ]
-
-
-
-console.log(bateaux);
+module.exports = m_liste_bateaux.js
