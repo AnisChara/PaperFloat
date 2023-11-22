63020d3f9d77dca4d759ab8ca09be09c16a65cbe
Vince <vmartin@et.esiea.fr>
2023-11-22 12:05:04 +0100

    module verification de tir

--- m_verification_tir.js
+++ m_verification_tir.js
@@ -2,38 +2,25 @@
 "use strict";
 
 // require module de anis (module_liste_bateaux)
-const bateau = require("m_liste_bateaux");
+const bateaux = require("./m_liste_bateaux.js");
 // require module de tir 
-
+let tir = {x: 0, y: 0}
 // créer une fonction 
 const verification_tir = function ()
 {
-	// lire la grille 
-	//for ( let i = 0; i < 9; i++ ) 
-	//{
-	//	for ( let j = 0; i < 9; i++ ) 
-	//	{	
-		// il faut vérifier si la colonne cliquée par l'utilisateur, si il y a un bateaux ou pas
-			if ( user === tir )
+	// je rentre pour commencer dans la liste bateaux où il y tout mes bateaux 
+	for ( let i = 0; i < bateaux.length; i++)
+	{
+		// ici je rentre dans la liste d'un des différent bateaux où j'ai mes différentes coordonées
+		for ( let u = 0; u < bateaux[i].length; u++)
+		{
+			// si le tir en x, y va être égal au coordonée du bateaux en x et y alors on change le statut en "true"
+			if (tir.x === bateaux[i][u].x && tir.y === bateaux[i][u].y)
 			{
-				// si l'utilisateur tir et qu'il y a un bateau (coordonée du bateau)
-				else if ( tir === bateau )
-				{
-					//il y a un bateau,renvoyer "true" puis afficher autre module qui permet d'afficher la case qui est touchée
-					bateau = true; 
-					console.log("il y a un bateau"); 		
-				}
-				// si l'utilisateur tir et qu'il n'y a pas de bateau(coordonée du bateau)
-				else if ( tir !== bateau )
-				{
-					//il n'y a pas de bateau,renvoyer "false" puis ne rien afficher
-					bateau = false; 
-					console.log("il n'y a pas de bateau"); 		
-				}
+				bateaux[i][u].state = true; 
 			}
 		}
 	
 	}
-
 }
 module.exports = verification_tir;
