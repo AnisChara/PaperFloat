c36b512e696cc60f79c809f600ae7481fb489a2c
Chara Anis <chara@et.esiea.fr>
2023-11-23 09:50:28 +0100

    patience est mère de vertue (ajout des comms)

--- m_placement_bateau.js
+++ m_placement_bateau.js
@@ -1,15 +1,19 @@
 "use strict"
 
 const m_placement_bateau = function (co_case_choisi, bateau_choisi, rotate)
-{
+{   
+    //Assignation des coordonnées de la case choisie à la premiere case du bateau
     co_case_choisi.x = bateau_choisi[0].x; co_case_choisi.y = bateau_choisi[0].y;
 
+
+    //Assignation des autres cases du bateau selon la rotation
     if (rotate === false)
     
     {
         for (let i = 1; i< bateau_choisi.length; i++)
         {
             bateau_choisi[i].y = bateau_choisi[0].y + i;
+            bateau_choisi[i].x = bateau_choisi[0].x;
         }
     }
 
@@ -19,6 +23,7 @@ const m_placement_bateau = function (co_case_choisi, bateau_choisi, rotate)
         for (let i = 1; i< bateau_choisi.length; i++)
         {
             bateau_choisi[i].x = bateau_choisi[0].x + i;
+            bateau_choisi[i].y = bateau_choisi[0].y;
         }
     }
     
