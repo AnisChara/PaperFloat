80cdfbddf4aec6ef98937a990a1045b859593ae9
Axel <gasser@et.esiea.fr>
2023-11-29 09:15:54 +0100

    modification de la sauvegarde

--- m_fonction_sauvegarde.js
+++ m_fonction_sauvegarde.js
@@ -1,10 +1,10 @@
 const traitement_sauvegarde = function(...args) // fonction prend en paramètre "arguments" indice 0 sauvegarde, indice 1 fonction, indice 2 jusqu'au reste = paramètre de la fonction 
 {
-    for(let arg of ars)arguments.push;
+    for(let arg of args)arguments.push(args);
     
     save = JSON.parse(arguments[0]) // variable qui va récupérer le premier argument qui est l'élement de sauvegarde
 
-    let parameter = slice(2,arguments.length) // parameter va récupérer tous les autres paramètres destinée à la fonction
+    let parameter = slice(2,args.length) // parameter va récupérer tous les autres paramètres destinée à la fonction
 
     save = arguments[1](parameter); // modification de la sauvegarde par la fonction
 
