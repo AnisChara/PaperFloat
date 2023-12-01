d7d90d9b5e06df68ebf4e802fe682155d0b5b06b
ABDEL-AZIZ <ahmatmahammad@et.intechinfo.fr>
2023-12-01 20:29:50 +0100

    module de tir

---
+++ m_tir_case.js
@@ -0,0 +1,29 @@
+"use strict";
+//recuperer les modules 
+let save = require(".\m_fonction_sauvegarde.js");
+let serveur = require(".\index.js");
+let grid = require(".\req_grid.js")
+const url = require("url");
+const http = require("http");
+const { isUndefined } = require("util");
+const tir=function(serveur)
+{
+    if (query === isUndefined)
+    {
+    }
+    else
+    {
+        let co = query.bouton;
+        let coord = co.split("/");
+        for(let i = 0; i<10;i++)
+        {
+            for(let j = 0; j<10;j++)
+            {
+                if(i===coord[1] && j===coord[2]&& coord[3]=== false)
+                {
+                    state = true;
+                }
+            }
+        }
+    }
+}
