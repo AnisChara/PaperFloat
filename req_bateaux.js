a78d73824c5cacc0cdbc44c04df682abf9ad9491
Vince <vmartin@et.esiea.fr>
2023-12-06 11:55:05 +0100

    j'envoie le module page_placement.html, page_placement.css, req_bateaux PAS TOUCHE AU MODULE SVP J'AI PAS FINIS

---
+++ req_bateaux.js
@@ -0,0 +1,36 @@
+// Traitement de "req_commencer"
+
+"use strict";
+
+const fs = require("fs");
+const nunjucks = require("nunjucks");;
+
+const req_bateaux = function (req, res, query) {
+
+	let marqueurs;
+	let page;
+	let bouton;
+
+	// AFFICHAGE DE LA PAGE D'ACCUEIL
+
+	page = fs.readFileSync('page_placement.html', 'utf-8');
+
+    let grid = "";
+
+    for (let i = 0; i < 10; i++) {
+		for ( let j = 0; j < 10; j++ ) {
+			grid+= `<div class="btn"><a type="submit" href="/req_bateaux?bouton=${i}/${j}"><input type="button"></a></div>`
+		}
+    }
+
+	marqueurs = {};
+	marqueurs.erreur = "";
+	marqueurs.grid = grid;
+	page = nunjucks.renderString(page, marqueurs);
+
+	res.writeHead(200, { 'Content-Type': 'text/html' });
+	res.write(page);
+	res.end();
+};
+
+module.exports = req_bateaux;
