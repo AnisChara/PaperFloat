d11ea129b5c537723af39cd381b4ffae8a72f8cf
Vince <vmartin@et.esiea.fr>
2023-12-06 11:52:26 +0100

    dm

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ const req_afficher_formulaire_inscription = require("./req_afficher_formulaire_i
 const req_inscrire = require("./req_inscrire.js");
 const req_identifier = require("./req_identifier.js");
 const req_grid = require("./req_grid.js");
+const req_bateaux = require("./req_bateaux.js");
 
 const req_statique = require("./req_statique.js");
 const req_erreur = require("./req_erreur.js");
@@ -31,6 +32,9 @@ const traite_requete = function (req, res) {
 	requete = url.parse(req.url, true);
 	pathname = requete.pathname;
 	query = requete.query;
+
+	console.log("pathname : " + pathname);
+    console.log("query string (x/y) : " + query.bouton);
 
 	// ROUTEUR
 
@@ -52,8 +56,11 @@ const traite_requete = function (req, res) {
 			case '/req_grid':
 				req_grid(req,res,query)
 				break;
-			case `/req_afficher_accueil_jeu`:
+				case `/req_afficher_accueil_jeu`:
 				req_afficher_accueil_jeu(req,res,query)
+				break;
+				case '/req_bateaux':
+				req_bateaux(req, res, query)
 				break;
 			default:
 				req_statique(req, res, query);
