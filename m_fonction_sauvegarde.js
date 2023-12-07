2590de02c26492e2dc10632bed2020c0e191045d
Axel <gasser@et.esiea.fr>
2023-12-07 08:54:30 +0100

    amélioration, plus lisible et plus compacte de m_verif_all_down.js

--- m_fonction_sauvegarde.js
+++ m_fonction_sauvegarde.js
@@ -1,8 +1,12 @@
+const fs = require("fs");
+
 const traitement_sauvegarde = function(...args) // fonction prend en paramètre "arguments" indice 0 sauvegarde, indice 1 fonction, indice 2 jusqu'au reste = paramètre de la fonction 
 {
     for(let arg of args)arguments.push(args);
     
-    save = JSON.parse(arguments[0]) // variable qui va récupérer le premier argument qui est l'élement de sauvegarde
+    save = fs.readFileSync(arguments[0]);
+
+    save = JSON.parse(save) // variable qui va récupérer le premier argument qui est l'élement de sauvegarde
 
     let parameter = slice(2,args.length) // parameter va récupérer tous les autres paramètres destinée à la fonction
 
@@ -10,6 +14,7 @@ const traitement_sauvegarde = function(...args) // fonction prend en paramètre 
 
     JSON.stringify(save); // stringify pour sauvegarder les changements
 
+    fs.WriteFileSync(save,"UTF-8")
 }
 
 module.exports = traitement_sauvegarde // export de la fonction traitement_sauvegarde
