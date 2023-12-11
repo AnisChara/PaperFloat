d6a42bde69e97d205975f8c65bea4f448a6c6f52
Chara Anis <chara@et.esiea.fr>
2023-12-11 11:51:26 +0100

    placement bateau

--- req_grid.js
+++ req_grid.js
@@ -16,28 +16,33 @@ const req_grid = function (req, res, query) {
 	page = fs.readFileSync('page_jeu.html', 'utf-8');
 
     let grid = "";
+	let css;
 
-    for (let i = 0; i < 10; i++) {
-		for ( let j = 0; j < 10; j++ ) {
-			//if ( case_tir === eau_touche )
-			//{
-			//	grid+= `<div class="eau"><a type="submit" href="/req_grid?bouton=${i}/${j}"><input type="button"></a></div>`
-			//}
-			//else if ( case_tir === bateau_touche ) 
-			//{
-			//	grid+= `<div class="bateau_touche"><a type="submit" href="/req_grid?bouton=${i}/${j}"><input type="button"></a></div>`
-			//}
-			//else if ( case_tir === bateau_non_touche )
-			//{
-			//	grid+= `<div class="bateau_non_touche"><a type="submit" href="/req_grid?bouton=${i}/${j}"><input type="button"></a></div>`
-			//}
-			//else 
+    for (let i = 0; i < 10; i++) 
+	{
+		for ( let j = 0; j < 10; j++ ) 
+		{
+			for (let u= 0; u < bateaux.length; u++)
 			{
-				grid+= `<div class="btn"><a type="submit" href="/req_grid?bouton=${i}-${j}"><input type="button"></a></div>`
+				for (let v = 0; v < bateaux[u].length; v++)
+				{
+					if (bateaux[u][v].x === j && bateaux[u][v].y === i)
+					{
+						css = true;
+					}
+				}
 			}
+			if (css === true)
+			{
+				grid+= `<div class="btn2"><a type="submit" href="/req_bateaux?bouton=${i}/${j}"><input type="button"></a></div>`
+			}
+			else
+			{
+				grid+= `<div class="btn"><a type="submit" href="/req_bateaux?bouton=${i}/${j}"><input type="button"></a></div>`
+			}
+			css = false;
 		}
     }
-
 	marqueurs = {};
 	marqueurs.erreur = "";
 	marqueurs.grid = grid;
