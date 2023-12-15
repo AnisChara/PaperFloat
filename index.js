f9c15a54fb458b97e425bdd781eb8782b22b33d8
Vince <vmartin@et.esiea.fr>
2023-12-15 09:35:30 +0100

    modification erreur Aziz + page tir

--- index.js
+++ index.js
@@ -17,6 +17,7 @@ const req_identifier = require("./req_identifier.js");
 const req_grid = require("./req_grid.js");
 const req_bateaux = require("./req_bateaux.js");
 const m_liste_bateaux = require("./m_liste_bateaux.js");
+const req_tir = require("./req_tir.js");
 
 const req_statique = require("./req_statique.js");
 const req_erreur = require("./req_erreur.js");
@@ -60,6 +61,9 @@ const traite_requete = function (req, res) {
 				break;
 			case '/req_bateaux':
 				req_bateaux(req,res,query)
+				break;
+			case '/req_tir':
+				req_tir(req, res, query)
 				break;
 			default:
 				req_statique(req, res, query);
