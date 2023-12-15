86199a267eeced53a21389942eebbeae9703d97b
Axel <gasser@et.esiea.fr>
2023-12-15 15:23:46 +0100

    modification mais pas encore utilisable

--- m_fonction_sauvegarde.js
+++ m_fonction_sauvegarde.js
@@ -2,29 +2,43 @@ const fs = require("fs");
 
 const traitement_sauvegarde = function(...args) // fonction prend en paramètre "arguments" indice 0 sauvegarde, indice 1 fonction, indice 2 jusqu'au reste = paramètre de la fonction 
 {
-    for(let arg of args)arguments.push(args);
+    let argument = [];
+    for(let arg of args)
+    {
+        argument.push(arg);
+    }
     
-    save = fs.readFileSync(arguments[0]);
+    console.log(argument);
+    
+    let save = fs.readFileSync(argument[0]);
 
-    save = JSON.parse(save) // variable qui va récupérer le premier argument qui est l'élement de sauvegarde
+    save = JSON.parse(save); // variable qui va récupérer le premier argument qui est l'élement de sauvegarde
 
-    let parameter = slice(2,args.length) // parameter va récupérer tous les autres paramètres destinée à la fonction
+    console.log(save);
 
-    save = arguments[1](parameter); // modification de la sauvegarde par la fonction
+    let parameter = argument.slice(2,args.length); // parameter va récupérer tous les autres paramètres destinée à la fonction
 
-    JSON.stringify(save); // stringify pour sauvegarder les changements
+    console.log(parameter);
+    console.log(argument[1]);
 
-    fs.writeFileSync(arguments[0],save,"UTF-8")
+    //let fct = argument[1];
+    //fct = function(save,parameter);
+    save = argument[1](save,parameter); // modification de la sauvegarde par la fonction
+
+    console.log(save);
+
+    save = JSON.stringify(save); // stringify pour sauvegarder les changements
+
+ fs.writeFileSync(argument[0],save,"UTF-8");
 }
 
-module.exports = traitement_sauvegarde // export de la fonction traitement_sauvegarde
+module.exports = traitement_sauvegarde; // export de la fonction traitement_sauvegarde
 
 const create_test_save = function()
 {
-    const json_test = 10;
-    String(json_test);
-    JSON.stringify(json_test);
-    fs.writeFileSync("./nb_test",json_test,"UTF-8");
+    let json_test = 10;
+    json_test = JSON.stringify(json_test);
+    fs.writeFileSync("./nb_test.json",json_test,"UTF-8");
 }
 
 const exe = function(nbBase,nb1,nb2,nb3,nb4)
@@ -35,7 +49,7 @@ const exe = function(nbBase,nb1,nb2,nb3,nb4)
     nbBase = Number(nbBase **nb4);
 }
 
-create_test_save();
-traitement_sauvegarde("./nb_test",exe,"2","3","3","4");
-const show = fs.readFileSync("./nb_test");
+create_test_save(); 
+traitement_sauvegarde("./nb_test.json",exe,"2","3","3","4");
+const show = fs.readFileSync("./nb_test.json");
 console.log(show);
