19a20cbf0f8056d7fe1dd40c2739f1378f7813b8
Chara Anis <chara@et.esiea.fr>
2023-12-18 15:18:55 +0100

    je perd des cheveux a cause d'un bug

--- m_ultime_placement.js
+++ m_ultime_placement.js
@@ -8,14 +8,14 @@ const m_placement_bateau = require("./m_placement_bateau.js");
 const verification_position = require("./m_verif_placement_bateau_grille.js");
 const doublon = require("./m_verif_bateau_identique.js");
 
-const ultime_placement = function(player,party,co_case_choisi,rotate,bateau)
+const ultime_placement = function(id,co_case_choisi,rotate,bateau)
 {  
    
     
     let co_bateau;
 
     //je recupere la sauvegarde des bateaux selon le joueur et la partie
-    let bateaux = fs.readFileSync("./save_bateaux_"+player+"_"+party+".json");
+    let bateaux = fs.readFileSync("./save_bateaux_"+id+".json");
     bateaux = JSON.parse(bateaux);
     let nb_bateau = bateau;
     bateau = bateaux[bateau];
@@ -23,7 +23,7 @@ const ultime_placement = function(player,party,co_case_choisi,rotate,bateau)
     //execution des programmes
     co_bateau = m_placement_bateau (co_case_choisi,bateau, rotate);
     co_bateau = verification_position(co_bateau);
-    co_bateau = doublon(co_bateau,player,party, nb_bateau);
+    co_bateau = doublon(co_bateau,id, nb_bateau);
 
     //si un des modules renvoi une erreur
     if(co_bateau === false)
