0c7fce431eb841387aafb9a8f61bacfd10917757
Chara Anis <chara@et.esiea.fr>
2023-12-19 14:08:53 +0100

    ajt de l'id page regles

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ const req_inscrire = require("./req_inscrire.js");
 const req_identifier = require("./req_identifier.js");
 const req_bateaux = require("./req_bateaux.js");
 const req_tir = require("./req_tir.js");
+const req_afficher_regles = require("./req_afficher_regles.js");
 
 const req_statique = require("./req_statique.js");
 const req_erreur = require("./req_erreur.js");
@@ -61,6 +62,9 @@ const traite_requete = function (req, res) {
 				break;
 			case '/req_tir':
 				req_tir(req, res, query)
+				break;
+			case '/req_afficher_regles':
+				req_afficher_regles(req, res, query)
 				break;
 			default:
 				req_statique(req, res, query);
---
+++ req_afficher_regles.js
@@ -0,0 +1,35 @@
+"use strict"
+
+const fs = require("fs");
+const nunjucks = require("nunjucks");;
+
+const trait = function (req, res, query) {
+    
+    let marqueurs;
+	let page;
+
+	// AFFICHAGE DE LA modele_accueil_jeu
+
+	page = fs.readFileSync('page_regle.html', 'utf-8');
+
+	marqueurs = {};
+	marqueurs.erreur = "";
+	marqueurs.id = query.id;
+	console.log(query.id);
+	page = nunjucks.renderString(page, marqueurs);
+
+	res.writeHead(200, { 'Content-Type': 'text/html' });
+	res.write(page);
+	res.end();
+};
+
+module.exports = trait;
+
+
+
+
+
+
+
+
+
