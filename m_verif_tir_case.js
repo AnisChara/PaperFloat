b3a99d3dd0503206bc13328653a0439a4a056807
Chara Anis <chara@et.esiea.fr>
2023-12-21 18:00:56 +0100

    bug fix

--- m_verif_tir_case.js
+++ m_verif_tir_case.js
@@ -7,12 +7,12 @@ const verif_tir_case = function(pos,grid) // fonction qui regarde si la case a l
     let posX = pos.x; // récupération de la position X
     let posY = pos.y; // récupération de la position Y
 
-    if(grid[posY][posX] ===false) // si la case n'est pas touchée alors on la passe en true touchée
+    if(grid[posX][posY] ===false) // si la case n'est pas touchée alors on la passe en true touchée
     {
         return true; // return true "tir valide"
     }
 
-    else if(grid[posY][posX] ===true) // si la case n'est pas disponible
+    else if(grid[posX][posY] ===true) // si la case n'est pas disponible
     {
         return false; // return false "tir impossible"
     }
