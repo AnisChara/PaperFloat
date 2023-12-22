6a57a9f85ea037a8c3b04676df03850e4d134802
Vince <vmartin@et.esiea.fr>
2023-12-22 11:01:22 +0100

    case eau touchée

--- m_grid_publique.js
+++ m_grid_publique.js
@@ -6,43 +6,54 @@ const nunjucks = require("nunjucks");;
 const m_grid_publique = function( adverse,id) {
  
     let bateaux = JSON.parse(fs.readFileSync("./bateaux/save_bateaux_"+adverse+".json"))
+    let grille_t = JSON.parse(fs.readFileSync("./grille/save_grille_"+adverse+".json"))
     let grid = "";
 	let i;
 	let j;
-    let is_a_boat_touch = `<div class="btn2"><a type="submit" href="/req_tir?bouton=${i}-${j}&id=${id}&adverse=${adverse}"><input type="button"></a></div>`
-    let is_a_boat_down = `<div class="btn3"><a type="submit" href="/req_tir?bouton=${i}-${j}&id=${id}&adverse=${adverse}"><input type="button"></a></div>`
     let full_boat = 0;
 		
     for (i = 0; i < 10; i++)
     { 
         for (j = 0; j < 10; j++ )
         {
-			let css = `<div class="btn"><a type="submit" href="/req_tir?bouton=${i}-${j}&id=${id}&adverse=${adverse}"><input type="button"></a></div>`
+			let css = `<div class="case_eau"><a type="submit" href="/req_tir?bouton=${i}-${j}&id=${id}&adverse=${adverse}"><input type="button"></a></div>`
 
-            for (let u= 0; u < bateaux.length; u++)
+            if ( grille_t[j][i] === false )
             {
-                for (let v = 0; v < bateaux[u].length; v++)
+            }
+            else 
+            {
+               css = `<div class="case_eau_touch"><a type="submit" href="/req_tir?bouton=${i}-${j}&id=${id}&adverse=${adverse}"><input type="button"></a></div>`
+
+                for (let u= 0; u < bateaux.length; u++)
                 {
-                    if (bateaux[u][v].x === j && bateaux[u][v].y === i && bateaux[u][v].state === true )
+                    for (let v = 0; v < bateaux[u].length; v++)
                     {
-                        for (let a = 0; a< bateaux[u].length; a++)
-                        {
-                            if(bateaux[u][a].state === true) full_boat++;
+                        if (bateaux[u][v].x === j && bateaux[u][v].y === i && bateaux[u][v].state === true )
+                        {   
+                            for (let a = 0; a< bateaux[u].length; a++)
+                            {
+                                if(bateaux[u][a].state === true)
+                                {
+                                    full_boat++;
+                                }
+                            }
+                            if(full_boat === bateaux[u].length) 
+						    {
+						    	css = `<div class="case_bateau_touch_c"><a type="submit" href="/req_tir?bouton=${i}-${j}&id=${id}&adverse=${adverse}"><input type="button"></a></div>`
+						    }
+                            else
+						    {
+							    css = `<div class="case_bateau_touch"><a type="submit" href="/req_tir?bouton=${i}-${j}&id=${id}&adverse=${adverse}"><input type="button"></a></div>`
+						    }
+                    
+						    full_boat = 0;
                         }
-                        if(full_boat === bateaux[u].length) 
-						{
-							css = is_a_boat_down
-						}
-                        else
-						{
-							css = is_a_boat_touch;
-						}
-						full_boat = 0;
                     }
-                }
+                }   
             }
                 grid += css;
-				css = `<div class="btn"><a type="submit" href="/req_tir?bouton=${i}-${j}&id=${id}&adverse=${adverse}"><input type="button"></a></div>`
+				css = `<div class="case_eau"><a type="submit" href="/req_tir?bouton=${i}-${j}&id=${id}&adverse=${adverse}"><input type="button"></a></div>`
 
         }
     }
