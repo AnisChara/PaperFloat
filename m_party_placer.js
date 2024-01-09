4c01aebd7c8deaf5a7fda524055165b19bd118d2
Chara Anis <chara@et.esiea.fr>
2024-01-09 21:22:06 +0100

    for aziz92i

--- m_party_placer.js
+++ m_party_placer.js
@@ -4,13 +4,13 @@ const fs = require("fs");
 
 const party_placer = function(id)
 {   
-    let list_party = JSON.parse(fs.readFileSync("./party.json"));
+    let list_party = JSON.parse(fs.readFileSync("./party.json"));// on prend la liste des parties en cours
     let adverse;
     let place;
 
-    for (let i = 0; i< list_party.length; i++)
+    for (let i = 0; i< list_party.length; i++)//on balaye la liste
     {   
-        if (list_party[i].player1 === id)
+        if (list_party[i].player1 === id)// Lors de la deuxieme tentative le joueur est deja placé on vérifie si quelqu'un est arrivé entre temps
         {
             adverse = list_party[i].player2;
             return adverse;
@@ -20,7 +20,7 @@ const party_placer = function(id)
             adverse = list_party[i].player1;
             return adverse;
         }
-        else if (list_party[i].player1 && !list_party[i].player2)
+        else if (list_party[i].player1 && !list_party[i].player2)//POV du 2eme joeuur; il se place si il ya de la place
         {
             list_party[i].player2 = id;
             adverse = list_party[i].player1;
@@ -41,16 +41,15 @@ const party_placer = function(id)
     }
     else if (!adverse)
     {
-        for (let i = 0; i< list_party.length; i++)
+        for (let i = 0; i< list_party.length; i++)//verifie une partie vide 
         {
             if (!list_party[i].player1)
             {
                 list_party[i].player1 = id;
                 place= true;
-                break;
             }
         }
-        if (place !== true)
+        if (place !== true)// il crée sa partie
         {
             list_party.push({ "player1" : id , "player2" : ""});
         }
