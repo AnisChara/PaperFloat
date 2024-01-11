5e8911db27a361ddded8bb26c2fefc4d50850c40
Axel <gasser@et.esiea.fr>
2024-01-11 18:14:47 +0100

    m_fonction_sauvegarde.js fonctionne correctement test_fonction_sauvegarde permet de vérifier la fonction principale je m'occupe de m_bot.classic.js merci de ne pas y toucher la team :>

--- m_fonction_sauvegarde.js
+++ m_fonction_sauvegarde.js
@@ -1,7 +1,12 @@
 const fs = require("fs");
 
-const traitement_sauvegarde = function(...args) // fonction prend en paramètre "arguments" indice 0 sauvegarde, indice 1 fonction, indice 2 jusqu'au reste = paramètre de la fonction 
-{
+/**
+ * fonction prend en paramètre "arguments" indice 0 sauvegarde, indice 1 fonction, indice 2 jusqu'au reste = paramètre de la fonction 
+ * @param  {string} filename name of the json file to load and save
+ * @param {Function} funct the callback function to execute. It recieve data from the json and following argument, the result of the callback is stored in the json file
+ * @param {...any} args the argument for the callback
+ */
+const traitement_sauvegarde = function(...args) {
     let argument = [];
     for(let arg of args)
     {
@@ -9,47 +14,28 @@ const traitement_sauvegarde = function(...args) // fonction prend en paramètre 
     }
     
     console.log(argument);
+    const filename = argument[0];
+    const funct = argument[1];
     
-    let save = fs.readFileSync(argument[0]);
 
-    save = JSON.parse(save); // variable qui va récupérer le premier argument qui est l'élement de sauvegarde
+    let save = JSON.parse(fs.readFileSync(filename)); // variable qui va récupérer le premier argument qui est l'élement de sauvegarde
 
     console.log(save);
 
-    let parameter = argument.slice(2,args.length); // parameter va récupérer tous les autres paramètres destinée à la fonction
+    let parameters = argument.slice(2,args.length); // parameter va récupérer tous les autres paramètres destinée à la fonction
 
-    console.log(parameter);
+    console.log(parameters);
     console.log(argument[1]);
 
     //let fct = argument[1];
     //fct = function(save,parameter);
-    save = argument[1](save,parameter); // modification de la sauvegarde par la fonction
+    
+    save = funct(save,...parameters); // modification de la sauvegarde par la fonction
 
     console.log(save);
 
-    save = JSON.stringify(save); // stringify pour sauvegarder les changements
-
- fs.writeFileSync(argument[0],save,"UTF-8");
+    // stringify pour sauvegarder les changements
+    fs.writeFileSync(filename,JSON.stringify(save),"UTF-8");
 }
 
 module.exports = traitement_sauvegarde; // export de la fonction traitement_sauvegarde
-
-const create_test_save = function()
-{
-    let json_test = 10;
-    json_test = JSON.stringify(json_test);
-    fs.writeFileSync("./nb_test.json",json_test,"UTF-8");
-}
-
-const exe = function(nbBase,nb1,nb2,nb3,nb4)
-{
-    nbBase = Number(nbBase + nb1);
-    nbBase = Number(nbBase - nb2);
-    nbBase = Number(nbBase / nb3);
-    nbBase = Number(nbBase **nb4);
-}
-
-create_test_save(); 
-traitement_sauvegarde("./nb_test.json",exe,"2","3","3","4");
-const show = fs.readFileSync("./nb_test.json");
-console.log(show);
