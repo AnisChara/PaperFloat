dbf9d29af1ba791458bb9183b43989773d4f3ecc
Chara Anis <chara@et.esiea.fr>
2024-01-24 21:25:51 +0100

    ajout de bateaux cassé

--- m_grid_prv_nc.js
+++ m_grid_prv_nc.js
@@ -143,122 +143,105 @@ const m_grid_nc = function(id) {
                         if (bateaux[u][v].x === j && bateaux[u][v].y === i)
                         { 
                             
-							for (let a = 0; a< bateaux[u].length; a++)
-							{
-								if(bateaux[u][a].state === true)
-								{
-									full_boat++;
-								}
-							}
-							if(full_boat === bateaux[u].length) 
-							{
-                                //TODO: decider si on fait un design different pour coulé
-								css = `<div class="case_bateau_touch_c"><a type="submit" href="/req_tir?bouton=${i}-${j}&id=${id}"><input type="button" disabled="disabled"></a></div>`
-							}
-							else
-							{
-                                if (u === 1 || u === 2)                           
-                                {
-                                    if (bateaux[u][0].x === bateaux[u][1].x)
-                                    {   
-                                        if (v === 0)
-                                        {
-                                            css = `<div class="case2_bateau3T"><a type="submit" href="/req_bateaux?bouton=${i}-${j}&id=${id}"><input type="button" disabled="disabled"></a></div>`
-                                        }
-                                        else if (v === 1)
-                                        {
-                                            css = `<div class="case1_bateau3T"><a type="submit" href="/req_bateaux?bouton=${i}-${j}&id=${id}"><input type="button" disabled="disabled"></a></div>`
-                                        }
-                                        else if (v === 2)
-                                        {
-                                            css = `<div class="case3_bateau3T"><a type="submit" href="/req_bateaux?bouton=${i}-${j}&id=${id}"><input type="button" disabled="disabled"></a></div>`
-                                        }
-                                    }
-                                    else if (bateaux[u][0].y === bateaux[u][1].y)
-                                    {
-                                        if (v === 0)
-                                        {
-                                            css = `<div class="case2_bateau3HT"><a type="submit" href="/req_bateaux?bouton=${i}-${j}&id=${id}"><input type="button" disabled="disabled"></a></div>`
-                                        }
-                                        else if (v === 1)
-                                        {
-                                            css = `<div class="case1_bateau3HT"><a type="submit" href="/req_bateaux?bouton=${i}-${j}&id=${id}"><input type="button" disabled="disabled"></a></div>`
-                                        }
-                                        else if (v === 2)
-                                        {
-                                            css = `<div class="case3_bateau3HT"><a type="submit" href="/req_bateaux?bouton=${i}-${j}&id=${id}"><input type="button" disabled="disabled"></a></div>`
-                                        }
-                                    }
-                                }
-                                else if (u === 3)
+                            if (u === 1 || u === 2)                           
+                            {
+                                if (bateaux[u][0].x === bateaux[u][1].x)
                                 {   
-                                    if (bateaux[u][0].y === bateaux[u][1].y)
+                                    if (v === 0)
                                     {
-                                        if (v === 0)
-                                        {
-                                            css = `<div class="case3_bateau5HT"><a type="submit" href="/req_bateaux?bouton=${i}-${j}&id=${id}"><input type="button" disabled="disabled"></a></div>`
-                                        }
-                                        else if (v === 1)
-                                        {
-                                            css = `<div class="case2_bateau5HT"><a type="submit" href="/req_bateaux?bouton=${i}-${j}&id=${id}"><input type="button" disabled="disabled"></a></div>`
-                                        }
-                                        else if (v === 2)
-                                        {
-                                            css = `<div class="case4_bateau5HT"><a type="submit" href="/req_bateaux?bouton=${i}-${j}&id=${id}"><input type="button" disabled="disabled"></a></div>`
-                                        }
-                                        else if (v === 3)
-                                        {
-                                            css = `<div class="case1_bateau5HT"><a type="submit" href="/req_bateaux?bouton=${i}-${j}&id=${id}"><input type="button" disabled="disabled"></a></div>`
-                                        }
-                                        else if (v === 4)
-                                        {
-                                            css = `<div class="case5_bateau5HT"><a type="submit" href="/req_bateaux?bouton=${i}-${j}&id=${id}"><input type="button" disabled="disabled"></a></div>`
-                                        }
+                                        css = `<div class="case2_bateau3T"><a type="submit" href="/req_bateaux?bouton=${i}-${j}&id=${id}"><input type="button" disabled="disabled"></a></div>`
                                     }
-                                    if (bateaux[u][0].x === bateaux[u][1].x)
+                                    else if (v === 1)
                                     {
-                                        if (v === 0)
-                                        {
-                                            css = `<div class="case3_bateau5T"><a type="submit" href="/req_bateaux?bouton=${i}-${j}&id=${id}"><input type="button" disabled="disabled"></a></div>`
-                                        }
-                                        else if (v === 1)
-                                        {
-                                            css = `<div class="case2_bateau5T"><a type="submit" href="/req_bateaux?bouton=${i}-${j}&id=${id}"><input type="button" disabled="disabled"></a></div>`
-                                        }
-                                        else if (v === 2)
-                                        {
-                                            css = `<div class="case4_bateau5T"><a type="submit" href="/req_bateaux?bouton=${i}-${j}&id=${id}"><input type="button" disabled="disabled"></a></div>`
-                                        }
-                                        else if (v === 3)
-                                        {
-                                            css = `<div class="case1_bateau5T"><a type="submit" href="/req_bateaux?bouton=${i}-${j}&id=${id}"><input type="button" disabled="disabled"></a></div>`
-                                        }
-                                        else if (v === 4)
-                                        {
-                                            css = `<div class="case5_bateau5T"><a type="submit" href="/req_bateaux?bouton=${i}-${j}&id=${id}"><input type="button" disabled="disabled"></a></div>`
-                                        }
+                                        css = `<div class="case1_bateau3T"><a type="submit" href="/req_bateaux?bouton=${i}-${j}&id=${id}"><input type="button" disabled="disabled"></a></div>`
+                                    }
+                                    else if (v === 2)
+                                    {
+                                        css = `<div class="case3_bateau3T"><a type="submit" href="/req_bateaux?bouton=${i}-${j}&id=${id}"><input type="button" disabled="disabled"></a></div>`
                                     }
                                 }
-                                else if (u === 0)
+                                else if (bateaux[u][0].y === bateaux[u][1].y)
                                 {
-                                    css = `<div class="case_bateau1T"><a type="submit" href="/req_bateaux?bouton=${i}-${j}&id=${id}"><input type="button" disabled="disabled"></a></div>`
+                                    if (v === 0)
+                                    {
+                                        css = `<div class="case2_bateau3HT"><a type="submit" href="/req_bateaux?bouton=${i}-${j}&id=${id}"><input type="button" disabled="disabled"></a></div>`
+                                    }
+                                    else if (v === 1)
+                                    {
+                                        css = `<div class="case1_bateau3HT"><a type="submit" href="/req_bateaux?bouton=${i}-${j}&id=${id}"><input type="button" disabled="disabled"></a></div>`
+                                    }
+                                    else if (v === 2)
+                                    {
+                                        css = `<div class="case3_bateau3HT"><a type="submit" href="/req_bateaux?bouton=${i}-${j}&id=${id}"><input type="button" disabled="disabled"></a></div>`
+                                    }
                                 }
-                                else if (u === 4)
+                            }
+                            else if (u === 3)
+                            {   
+                                if (bateaux[u][0].y === bateaux[u][1].y)
                                 {
-                                    if(v === 0) css = `<div class="case0_bateau9T"><a type="submit" href="/req_bateaux?bouton=${i}-${j}&id=${id}"><input type="button" disabled="disabled"></a></div>`
-                                    else if (v === 1) css = `<div class="caseTop_bateau9T"><a type="submit" href="/req_bateaux?bouton=${i}-${j}&id=${id}"><input type="button" disabled="disabled"></a></div>`
-                                    else if (v === 2) css = `<div class="caseBottom_bateau9T"><a type="submit" href="/req_bateaux?bouton=${i}-${j}&id=${id}"><input type="button" disabled="disabled"></a></div>`
-                                    else if (v === 3) css = `<div class="caseLeft_bateau9T"><a type="submit" href="/req_bateaux?bouton=${i}-${j}&id=${id}"><input type="button" disabled="disabled"></a></div>`
-                                    else if (v === 4) css = `<div class="caseRight_bateau9T"><a type="submit" href="/req_bateaux?bouton=${i}-${j}&id=${id}"><input type="button" disabled="disabled"></a></div>`
-                                    else if (v === 5) css = `<div class="caseBottomRight_bateau9T"><a type="submit" href="/req_bateaux?bouton=${i}-${j}&id=${id}"><input type="button" disabled="disabled"></a></div>`
-                                    else if (v === 6) css = `<div class="caseBottomLeft_bateau9T"><a type="submit" href="/req_bateaux?bouton=${i}-${j}&id=${id}"><input type="button" disabled="disabled"></a></div>`
-                                    else if (v === 7) css = `<div class="caseTopRight_bateau9T"><a type="submit" href="/req_bateaux?bouton=${i}-${j}&id=${id}"><input type="button" disabled="disabled"></a></div>`
-                                    else if (v === 8) css = `<div class="caseTopLeft_bateau9T"><a type="submit" href="/req_bateaux?bouton=${i}-${j}&id=${id}"><input type="button" disabled="disabled"></a></div>`                            
-                                }					                                
+                                    if (v === 0)
+                                    {
+                                        css = `<div class="case3_bateau5HT"><a type="submit" href="/req_bateaux?bouton=${i}-${j}&id=${id}"><input type="button" disabled="disabled"></a></div>`
+                                    }
+                                    else if (v === 1)
+                                    {
+                                        css = `<div class="case2_bateau5HT"><a type="submit" href="/req_bateaux?bouton=${i}-${j}&id=${id}"><input type="button" disabled="disabled"></a></div>`
+                                    }
+                                    else if (v === 2)
+                                    {
+                                        css = `<div class="case4_bateau5HT"><a type="submit" href="/req_bateaux?bouton=${i}-${j}&id=${id}"><input type="button" disabled="disabled"></a></div>`
+                                    }
+                                    else if (v === 3)
+                                    {
+                                        css = `<div class="case1_bateau5HT"><a type="submit" href="/req_bateaux?bouton=${i}-${j}&id=${id}"><input type="button" disabled="disabled"></a></div>`
+                                    }
+                                    else if (v === 4)
+                                    {
+                                        css = `<div class="case5_bateau5HT"><a type="submit" href="/req_bateaux?bouton=${i}-${j}&id=${id}"><input type="button" disabled="disabled"></a></div>`
+                                    }
+                                }
+                                if (bateaux[u][0].x === bateaux[u][1].x)
+                                {
+                                    if (v === 0)
+                                    {
+                                        css = `<div class="case3_bateau5T"><a type="submit" href="/req_bateaux?bouton=${i}-${j}&id=${id}"><input type="button" disabled="disabled"></a></div>`
+                                    }
+                                    else if (v === 1)
+                                    {
+                                        css = `<div class="case2_bateau5T"><a type="submit" href="/req_bateaux?bouton=${i}-${j}&id=${id}"><input type="button" disabled="disabled"></a></div>`
+                                    }
+                                    else if (v === 2)
+                                    {
+                                        css = `<div class="case4_bateau5T"><a type="submit" href="/req_bateaux?bouton=${i}-${j}&id=${id}"><input type="button" disabled="disabled"></a></div>`
+                                    }
+                                    else if (v === 3)
+                                    {
+                                        css = `<div class="case1_bateau5T"><a type="submit" href="/req_bateaux?bouton=${i}-${j}&id=${id}"><input type="button" disabled="disabled"></a></div>`
+                                    }
+                                    else if (v === 4)
+                                    {
+                                        css = `<div class="case5_bateau5T"><a type="submit" href="/req_bateaux?bouton=${i}-${j}&id=${id}"><input type="button" disabled="disabled"></a></div>`
+                                    }
+                                }
+                            }
+                            else if (u === 0)
+                            {
+                                css = `<div class="case_bateau1T"><a type="submit" href="/req_bateaux?bouton=${i}-${j}&id=${id}"><input type="button" disabled="disabled"></a></div>`
+                            }
+                            else if (u === 4)
+                            {
+                                if(v === 0) css = `<div class="case0_bateau9T"><a type="submit" href="/req_bateaux?bouton=${i}-${j}&id=${id}"><input type="button" disabled="disabled"></a></div>`
+                                else if (v === 1) css = `<div class="caseTop_bateau9T"><a type="submit" href="/req_bateaux?bouton=${i}-${j}&id=${id}"><input type="button" disabled="disabled"></a></div>`
+                                else if (v === 2) css = `<div class="caseBottom_bateau9T"><a type="submit" href="/req_bateaux?bouton=${i}-${j}&id=${id}"><input type="button" disabled="disabled"></a></div>`
+                                else if (v === 3) css = `<div class="caseLeft_bateau9T"><a type="submit" href="/req_bateaux?bouton=${i}-${j}&id=${id}"><input type="button" disabled="disabled"></a></div>`
+                                else if (v === 4) css = `<div class="caseRight_bateau9T"><a type="submit" href="/req_bateaux?bouton=${i}-${j}&id=${id}"><input type="button" disabled="disabled"></a></div>`
+                                else if (v === 5) css = `<div class="caseBottomRight_bateau9T"><a type="submit" href="/req_bateaux?bouton=${i}-${j}&id=${id}"><input type="button" disabled="disabled"></a></div>`
+                                else if (v === 6) css = `<div class="caseBottomLeft_bateau9T"><a type="submit" href="/req_bateaux?bouton=${i}-${j}&id=${id}"><input type="button" disabled="disabled"></a></div>`
+                                else if (v === 7) css = `<div class="caseTopRight_bateau9T"><a type="submit" href="/req_bateaux?bouton=${i}-${j}&id=${id}"><input type="button" disabled="disabled"></a></div>`
+                                else if (v === 8) css = `<div class="caseTopLeft_bateau9T"><a type="submit" href="/req_bateaux?bouton=${i}-${j}&id=${id}"><input type="button" disabled="disabled"></a></div>`                            
+                            }					                                
 
-							}
-						
-						    full_boat = 0;
                         }
                     }
                 }   
